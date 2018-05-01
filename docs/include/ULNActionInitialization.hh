610e18d8527c867418f0feac1ddec2c637cbf6a0
MrEBK38 <35082455+Mosley38@users.noreply.github.com>
2018-05-01 13:26:01 -0500

    Create ULNActionInitialization.hh

---
+++ docs/include/ULNActionInitialization.hh
@@ -0,0 +1,19 @@
+//
+//action initialization
+
+#ifndef ULNActionInitialization_h
+#define ULNActionInitialization_h 1
+
+#include "G4VUserActionInitialization.hh"
+
+class ULNActionInitialization : public G4VUserActionInitialization
+{
+public:
+	ULNActionInitialization();
+	virtual ~ULNActionInitialization();
+	virtual void BuildForMaster() const;
+	virtual void Build() const;
+
+};
+
+#endif
